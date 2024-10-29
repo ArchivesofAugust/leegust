cabc67324c7356f9bcce580efdf021e72a060961
Yongwoo, Lee (Personal) <archivesofaugust@gmail.com>
2024-10-29 22:48:57 +0900

    Modified description

--- libs/home/feature-layout/HomeLayout.tsx
+++ libs/home/feature-layout/HomeLayout.tsx
@@ -12,7 +12,7 @@ const cx = classNames.bind(styles)
 
 const HomeLayout = () => {
   const title = 'Archives of August'
-  const description = 'I write code, love design, and explore the spaces in between.'
+  const description = 'I write code, love design, and explore the space in between.'
 
   const socials: { icon: IconName; href: string }[] = [
     {
