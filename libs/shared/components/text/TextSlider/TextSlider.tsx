1056edd44cf480a9059469c2ddc1a62cdcd6af72
Yongwoo, Lee (Personal) <archivesofaugust@gmail.com>
2024-11-02 23:32:39 +0900

    Updated

--- libs/shared/components/text/TextSlider/TextSlider.tsx
+++ libs/shared/components/text/TextSlider/TextSlider.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useEffect, useRef } from 'react'
+import { useRef } from 'react'
 
 import classNames from 'classnames/bind'
 
@@ -17,15 +17,6 @@ const TextSlider = ({ value, hoverValue = 'Yongwoo' }: Props) => {
   const defaultRef = useRef<HTMLSpanElement>(null)
   const hoverRef = useRef<HTMLSpanElement>(null)
   const containerRef = useRef<HTMLSpanElement>(null)
-
-  useEffect(() => {
-    if (defaultRef.current && hoverRef.current && containerRef.current) {
-      const defaultWidth = defaultRef.current.offsetWidth
-      const hoverWidth = hoverRef.current.offsetWidth
-      const maxWidth = Math.max(defaultWidth, hoverWidth) * 2 // 5% 여유
-      containerRef.current.style.width = `${maxWidth}px`
-    }
-  }, [value, hoverValue])
 
   return (
     <span className={cx('wrapper')}>
