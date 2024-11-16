57df066d2a9376749e67b044f85ab1411e6a7274
Yongwoo, Lee (Personal) <archivesofaugust@gmail.com>
2024-11-16 19:46:40 +0900

    Removed notes

--- libs/home/feature-layout/HomeLayout.tsx
+++ libs/home/feature-layout/HomeLayout.tsx
@@ -15,10 +15,6 @@ const HomeLayout = () => {
 
   const items: { name: string; href: string }[] = [
     {
-      name: 'notes',
-      href: 'https://www.notion.so/Notes-Archives-of-August-13205b4b915e80408957e5e8b2a04768?pvs=4',
-    },
-    {
       name: 'about',
       href: 'https://www.notion.so/About-13205b4b915e8011a730c033d17faf4d?pvs=4',
     },
